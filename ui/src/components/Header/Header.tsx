2e61726fadc4f3281d086d19f903fb1b0c89d5e6
Antonios Papadopoulos <antonios.papadopoulos@nexthink.com>
2024-10-14 12:42:17 +0200

    chore(ui): make header sticky

--- ui/src/components/Header/Header.tsx
+++ ui/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className="bg-[#001529] text-white p-5">
+    <header className="bg-[#001529] text-white p-5 sticky top-0">
       <h1 className="text-md font-bold text-center">
         INSPIREHEP Search & Summarization Web App
       </h1>
