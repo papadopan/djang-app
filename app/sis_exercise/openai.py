8d8c3707b71773f7b3e2dec5235a17280795f1bb
Antonios Papadopoulos <antonios.papadopoulos@nexthink.com>
2024-10-14 09:30:20 +0200

    feat(app): intro of openai summary function

---
+++ app/sis_exercise/openai.py
@@ -0,0 +1,33 @@
+import requests
+
+OPENAI_API_KEY = "sk-ajckfxo0yK_potdZEFZgalO8Gwov1XJ9rJGBArRSYqT3BlbkFJUurgPuc1U161SbQl1PKhgS4FPf9TmsP9-UYzkIFasA"
+
+def generate_summary_text(prompt):
+    """This function generates a summary of the given text using OpenAI's GPT-4o model."""
+
+    headers = {
+        "Authorization": f"Bearer {OPENAI_API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "model":"gpt-4o",
+        "messages":[
+            {"role":"user","content": f"Summarize the following text: {prompt}"}
+        ]
+    }
+
+    try:
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data, verify=False)
+
+        # validate the response was successful
+        response.raise_for_status()
+
+        # extract the summary from the response
+        summary = response.json().get("choices",[{}])[0].get("message", {}).get("content", "")
+
+        return summary
+
+    except Exception as e:
+        return f"An error occurred while getting response from openai: {e}"
+    
