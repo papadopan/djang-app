6c9fd0174da69ac0a551ac90d2fc5b99e7f6d732
Antonios Papadopoulos <antonios.papadopoulos@nexthink.com>
2024-10-14 11:24:11 +0200

    chore(ui): init form/searchbar  structure

--- ui/src/components/Main/Main.tsx
+++ ui/src/components/Main/Main.tsx
@@ -1,3 +1,18 @@
+import { SearchBar } from "../SearchBar/SearchBar";
+
 export const Main = () => {
-  return <div>djhsjkhjk</div>;
+  return (
+    <section className="py-6 px-10">
+      <>
+        <h1 className="text-lg font-bold">
+          Discover High-Energy Physics Content
+        </h1>
+        <p className="text-sm text-gray-600">
+          INSPIRE is a trusted community hub that helps researchers to share and
+          find accurate scholarly information in high energy physics
+        </p>
+      </>
+      <SearchBar />
+    </section>
+  );
 };
---
+++ ui/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,46 @@
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const searchBarSchema = z.object({
+  query: z.string().optional(),
+});
+
+export const SearchBar = ({
+  onFormSubmit,
+  isPending,
+}: {
+  onFormSubmit: (query: string) => void;
+  isPending: boolean;
+}) => {
+  const form = useForm<z.infer<typeof searchBarSchema>>({
+    resolver: zodResolver(searchBarSchema),
+    defaultValues: {
+      query: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof searchBarSchema>) => {
+    onFormSubmit(values.query ?? "");
+  };
+
+  return (
+    <form
+      onSubmit={form.handleSubmit(onSubmit)}
+      className="flex justify-center gap-4 my-4"
+    >
+      <input
+        {...form.register("query")}
+        className="w-full p-2  rounded border border-[#001529]"
+        placeholder="Search for articles"
+      />
+      <button
+        type="submit"
+        disabled={isPending}
+        className="bg-[#001529] text-white hover:bg-[#001529]/90 py-3 px-4  disabled:cursor-not-allowed rounded disabled:bg-[#001529]/10"
+      >
+        search
+      </button>
+    </form>
+  );
+};
